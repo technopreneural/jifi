97b6d25e10b0b369f07e84ad83507bfbc6db12a8
technopreneural <technopreneural@yahoo.com>
2020-03-05 22:10:43 +0800

    build/debug scripts

--- hostapd/build.sh
+++ hostapd/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-tar czvf hostapd.tar *
+tar czvf hostapd.tar --exclude={*.hostapd,*.sh,script2} *
 docker build -f Dockerfile.hostapd -t hostapd .
 rm hostapd.tar
 docker image prune -f
--- hostapd/debug.sh
+++ hostapd/debug.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 [[ -z "${1}" ]] && NAME=hostapd || NAME="${1}"
-docker run --name "${NAME}" --net=host --cap-add=NET_ADMIN  --device=/dev/rfkill -v script:/home/app/script -it --entrypoint=bash "${NAME}"
+docker run --name "${NAME}" --net=host --cap-add=NET_ADMIN  --device=/dev/rfkill -v -it --entrypoint=bash "${NAME}"
