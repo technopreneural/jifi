3f10fc8c899263c12f88c2856c9e33c9e955c098
technopreneural <technopreneural@yahoo.com>
2020-03-10 23:24:52 +0800

    exec form entrypoint, specify files included in tarball

--- hostapd/build.sh
+++ hostapd/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-tar czvf hostapd.tar --exclude={*.hostapd,*.sh,script2} *
+tar czvf hostapd.tar script config data
 docker build -f Dockerfile.hostapd -t hostapd .
 rm hostapd.tar
 docker image prune -f
